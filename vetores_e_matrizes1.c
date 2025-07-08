c5b755ffaf543ef3c80ae207b731b9a80d05abeb
douglasestacio <douglasmyself@outlook.com>
2025-07-08 17:39:34 +0000

    Adicionando ou atualizando arquivos

---
+++ vetores_e_matrizes1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+
+    int mano[3];
+
+    mano[0] = 10;
+    mano[1] = 20;
+    mano[2] = 30;
+
+    printf ("Teste: %d\n", mano[2]);
+
+    printf ("Teste: %d\n", mano[0]);
+
+
+
+    return 0;
+}
